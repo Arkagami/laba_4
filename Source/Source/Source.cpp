b591b8e533f08d9867ebc563808642a702a73d9b
Arkagami <mixno20@gmail.com>
2017-06-05 17:22:30 +0300

    Закончил

--- Source/Source/Source.cpp
+++ Source/Source/Source.cpp
@@ -1,5 +1,46 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <windows.h>
+
+int StrToInt(char *ss) {
+	int i = 0, ii = 0;
+	char *s = ss;
+	while (*s) {
+		ii = i;
+		if (*s == '0') { i = 10 * i + 0; }
+		if (*s == '1') { i = 10 * i + 1; }
+		if (*s == '2') { i = 10 * i + 2; }
+		if (*s == '3') { i = 10 * i + 3; }
+		if (*s == '4') { i = 10 * i + 4; }
+		if (*s == '5') { i = 10 * i + 5; }
+		if (*s == '6') { i = 10 * i + 6; }
+		if (*s == '7') { i = 10 * i + 7; }
+		if (*s == '8') { i = 10 * i + 8; }
+		if (*s == '9') { i = 10 * i + 9; }
+		s++;
+		//if (ii > i) return -1;
+	}
+	return i;
+}
+
+int intLi(char s[1000]) {
+	int w = 0, dot = 0;
+	char chisla[11] = "0123456789";
+	while (s[w]) {
+		for (int i = 0;i < 10;i++) {
+			if (s[w] == chisla[i]) goto yes;
+			if (s[w] == '.') {
+				dot++;
+				if (dot == 2) return 0;
+				goto yes;
+			}
+		}
+		return 0;
+	yes:
+		w++;
+	}
+	return 1;
+}
 
 void code() {
   FILE *fin = fopen("input.txt", "r");
@@ -134,8 +175,31 @@ void decode() {
 
 int main()
 {
-  code();
-  decode();
+	int com = -1,	 // Number of command? I don't remember
+		u = -20,	 // Just needed
+		kolKom = 2;	 // Count of commands
+	char s[1000];
+	while (1) {
+		com = -1;
+		u = -20;
+		printf("Input command:\n");
+		printf("0 - Code\n");
+		printf("1 - Decode\n");
+		s[0] = '\0'; gets_s(s, 999);
+		u = StrToInt(s);
+		while ((intLi(s) == 0) || (u == -1)) {
+		repeat:;
+			printf("Try again:");
+			s[0] = '\0'; gets_s(s, 999);
+			u = StrToInt(s);
+		}
+		system("cls");
+		if (u > kolKom - 1) goto repeat; else
+		if (u == 0) code();
+		if (u == 1) decode();
+		u = -1;
+	}
+
     return 0;
 }
 
